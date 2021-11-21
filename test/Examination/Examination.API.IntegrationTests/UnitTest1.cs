a51d4fe40ad862d8676e5428f97914dcdd2f62d3
HuyDang504 <huy.dang@bbv.vn>
2021-11-21 18:59:25 +0700

    update-solution

---
+++ test/Examination/Examination.API.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Examination.API.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/Identity/Identity.API.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Identity.API.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
